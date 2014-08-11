16daef3b56bcf9b79f489999fedf83c6f07572bd
Adriano Dadario <adriano.dadario@vagas.com.br>
2014-08-11 12:26:04 -0300

    Coverting date from java to ruby

--- lib/hotdate/date_util.rb
+++ lib/hotdate/date_util.rb
@@ -1,6 +1,12 @@
 class DateUtil
 
   def self.hotdate?(ship_by_date, order_date)
+
+    diff_date = (ship_by_date - order_date).to_i
+    return true if order_date.friday? && diff_date <= 4
+    return true if order_date.saturday? && diff_date <= 3
+    return true if diff_date <= 1
+
     false
   end
 
--- spec/hotdate/date_util_spec.rb
+++ spec/hotdate/date_util_spec.rb
@@ -2,29 +2,93 @@ require_relative '../spec_helper'
 
 describe DateUtil do
 
+  def a_date(year, month, day)
+    Date.strptime("#{year}-#{month}-#{day}", '%Y-%m-%d')
+  end
+
 
   context 'TestOridinaryHotDate' do
 
-
-    it 'Error: Ordinary Date is not a Hot Date.' do
-      ship_by_date = Date.new(2011, 6, 8) # Friday, July 8 th 2011
-      order_date = Date.new(2011, 6, 6) # Wednesday, July 6 th 2011
+    it 'Ordinary Date is not a Hot Date.' do
+      ship_by_date = a_date(2011, 7, 8) # Friday, July 8 th 2011
+      order_date = a_date(2011, 7, 6) # Wednesday, July 6 th 2011
       expect(DateUtil.hotdate?(ship_by_date, order_date)).to be_falsy
     end
 
-    it 'Error: Ordinary Date is not a Hot Date.' do
-      ship_by_date = Date.new(2011, 6, 6) # Wednesday, July 6 th 2011
-      order_date = Date.new(2011, 6, 4) # Monday, July 4 th 2011
+    it 'Ordinary Date is not a Hot Date.' do
+      ship_by_date = a_date(2011, 7, 6) # Wednesday, July 6 th 2011
+      order_date = a_date(2011, 7, 4) # Monday, July 4 th 2011
       expect(DateUtil.hotdate?(ship_by_date, order_date)).to be_falsy
     end
 
   end
 
-  it 'TestBasicHotDate'
+  describe 'TestBasicHotDate' do
 
-  it 'TestHotDateFriday'
+    it 'Same Day Date is a Hot Date.' do
+      ship_by_date = a_date(2011, 7, 5); # Tuesday, July 5th 2011
+      order_date = a_date(2011, 7, 4); # Monday, July 4th 2011
 
-  it 'TestHotDateSaturday'
+      expect(DateUtil.hotdate?(ship_by_date, order_date)).to be_truthy
+    end
+
+    it 'Next Day Date is a Hot Date' do
+      ship_by_date = a_date(2011, 7, 4); # Monday, July 4th 2011
+      order_date = a_date(2011, 7, 4); # Monday, July 4th 2011
+      expect(DateUtil.hotdate?(ship_by_date, order_date)).to be_truthy
+    end
+
+    it 'Two Day Date is not a Hot Date' do
+      ship_by_date = a_date(2011, 7, 6); # Wednesday, July 6th 2011
+      order_date = a_date(2011, 7, 4); # Monday, July 4th 2011
+
+      expect(DateUtil.hotdate?(ship_by_date, order_date)).to be_falsy
+    end
+
+  end
+
+  describe 'TestHotDateFriday' do
+
+    let(:order_date) { a_date(2011, 7, 1) } # Friday, July 1 st 2011
+    let(:ship_by_date) { a_date(2011, 7, 1) } # Friday, July 1 st 2011
+
+    it 'Friday is a Hot Date' do
+      expect(DateUtil.hotdate?(ship_by_date, order_date)).to be_truthy
+    end
+
+    it 'Saturday is a Hot Date.' do
+      # Saturday, July 2 nd 2011
+      expect(DateUtil.hotdate?(ship_by_date + 1, order_date)).to be_truthy
+    end
+    it 'Sunday is a Hot Date' do
+      # Sunday, July 3 rd 2011
+      expect(DateUtil.hotdate?(ship_by_date + 2, order_date)).to be_truthy
+    end
+
+    it 'Monday is a Hot Date' do
+      # Monday, July 4 th 2011
+      expect(DateUtil.hotdate?(ship_by_date + 3, order_date)).to be_truthy
+    end
+  end
+
+  describe 'TestHotDateSaturday' do
+
+    let(:order_date) { a_date(2011, 7, 2) } # Saturday, July 2 nd 2011
+    let(:ship_by_date) { ship_by_date = a_date(2011, 7, 2) } #  Saturday, July 2 nd 2011
+
+    it 'Saturday is a Hot Date' do
+      expect(DateUtil.hotdate?(ship_by_date, order_date))
+    end
+
+    it 'Sunday is a Hot Date' do
+      # Sunday, July 3 rd 2011
+      expect(DateUtil.hotdate?(ship_by_date + 1, order_date))
+    end
+
+    it 'Monday is a Hot Date' do
+      expect(DateUtil.hotdate?(ship_by_date + 2, order_date))
+    end
+  end
 
 
 end

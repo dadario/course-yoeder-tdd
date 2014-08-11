cc37790c6cee31d6016a0bb508ccf30cb0582735
Adriano Dadario <adriano.dadario@vagas.com.br>
2014-08-11 10:36:33 -0300

    Adding code

--- lib/hotdate/date_util.rb
+++ lib/hotdate/date_util.rb
@@ -1,6 +1,8 @@
 class DateUtil
 
-
+  def self.hotdate?(ship_by_date, order_date)
+    false
+  end
 
 
 end
--- spec/hotdate/date_util_spec.rb
+++ spec/hotdate/date_util_spec.rb
@@ -3,7 +3,28 @@ require_relative '../spec_helper'
 describe DateUtil do
 
 
-  it 'TestOridinaryHotDate'
+  context 'TestOridinaryHotDate' do
+
+
+    it 'Error: Ordinary Date is not a Hot Date.' do
+      ship_by_date = Date.new(2011, 6, 8) # Friday, July 8 th 2011
+      order_date = Date.new(2011, 6, 6) # Wednesday, July 6 th 2011
+      expect(DateUtil.hotdate?(ship_by_date, order_date)).to be_falsy
+    end
+
+    it 'Error: Ordinary Date is not a Hot Date.' do
+      ship_by_date = Date.new(2011, 6, 6) # Wednesday, July 6 th 2011
+      order_date = Date.new(2011, 6, 4) # Monday, July 4 th 2011
+      expect(DateUtil.hotdate?(ship_by_date, order_date)).to be_falsy
+    end
+
+  end
+
+  it 'TestBasicHotDate'
+
+  it 'TestHotDateFriday'
+
+  it 'TestHotDateSaturday'
 
 
 end
